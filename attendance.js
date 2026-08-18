0d0f37680bf120b078c04bcf4cd38fc044eaaf6b
philatthebeach <phil@thebeach.nz>
2026-08-18 16:49:11 +1200

    Create attendance.js

---
+++ attendance.js
@@ -0,0 +1,28 @@
+const attendanceCodes = {
+
+    "mon-s1": "111111",
+    "mon-s2": "222222",
+    "mon-s3": "",
+    "mon-s4": "",
+
+    "tue-s1": "",
+    "tue-s2": "",
+    "tue-s3": "",
+    "tue-s4": "",
+
+    "wed-s1": "",
+    "wed-s2": "",
+    "wed-s3": "",
+    "wed-s4": "",
+
+    "thu-s1": "",
+    "thu-s2": "",
+    "thu-s3": "",
+    "thu-s4": "",
+
+    "fri-s1": "",
+    "fri-s2": "",
+    "fri-s3": "",
+    "fri-s4": ""
+
+};
