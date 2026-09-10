acda5c137ae9b1efc927235760763e5d69a160ed
philatthebeach <phil@thebeach.nz>
2026-09-10 15:25:51 +1200

    Update attendance.js

--- attendance.js
+++ attendance.js
@@ -1,7 +1,7 @@
 const attendanceCodes = {
 
-    "mon-s1": "612161",
-    "mon-s2": "440627",
+    "mon-s1": "164605",
+    "mon-s2": "386728",
     "mon-s3": "",
     "mon-s4": "",
 
@@ -10,13 +10,13 @@ const attendanceCodes = {
     "tue-s3": "",
     "tue-s4": "",
 
-    "wed-s1": "452622",
-    "wed-s2": "",
+    "wed-s1": "286175",
+    "wed-s2": "904264",
     "wed-s3": "",
     "wed-s4": "",
 
-    "thu-s1": "",
-    "thu-s2": "",
+    "thu-s1": "336831",
+    "thu-s2": "150295",
     "thu-s3": "",
     "thu-s4": "",
 
